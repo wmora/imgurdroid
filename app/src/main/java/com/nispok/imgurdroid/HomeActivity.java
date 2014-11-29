d614b3e833da96ff861e3150cfc988d507279c64
William Mora <wilmor24@gmail.com>
2014-11-28 22:32:16 -0300

    initial commit

---
+++ app/src/main/java/com/nispok/imgurdroid/HomeActivity.java
@@ -0,0 +1,24 @@
+package com.nispok.imgurdroid;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.nispok.imgurdroid.fragments.HomeFragment;
+
+public class HomeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new HomeFragment())
+                    .commit();
+        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
