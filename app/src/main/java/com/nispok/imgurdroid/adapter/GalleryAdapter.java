16866e6ccb5d959898899780392bd831310bf632
William Mora <wilmor24@gmail.com>
2014-11-30 14:27:43 -0300

    image resize + cropping

--- app/src/main/java/com/nispok/imgurdroid/adapter/GalleryAdapter.java
+++ app/src/main/java/com/nispok/imgurdroid/adapter/GalleryAdapter.java
@@ -1,5 +1,7 @@
 package com.nispok.imgurdroid.adapter;
 
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.ImageView;
 import com.nispok.imgurdroid.R;
 import com.nispok.imgurdroid.models.GalleryResult;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -63,7 +66,35 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryV
 
             Picasso.with(itemView.getContext())
                     .load(galleryResult.getImageUrl())
+                    .transform(new ImageTransformation(itemView, galleryResult.getImageUrl()))
                     .into(galleryResultImage);
+        }
+    }
+
+    private static class ImageTransformation implements Transformation {
+
+        private View holder;
+        private String id;
+
+        public ImageTransformation(View holder, String id) {
+            this.holder = holder;
+            this.id = id;
+        }
+
+        @Override
+        public Bitmap transform(Bitmap source) {
+            int width = holder.getWidth();
+            Bitmap transformedBitmap = ThumbnailUtils.extractThumbnail(source, width,
+                    source.getHeight(), ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+            if (transformedBitmap != source) {
+                source.recycle();
+            }
+            return transformedBitmap;
+        }
+
+        @Override
+        public String key() {
+            return String.format("%s_%d", id, holder.getWidth());
         }
     }
 
